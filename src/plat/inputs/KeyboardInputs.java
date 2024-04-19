3f0b11d551c2ac83fa0d34cc1b88b9ad33803e3d
Brickmaster256 <142838286+Brickmaster256@users.noreply.github.com>
2024-04-19 10:33:29 -0600

    formated the switch case and created the mouseInput class to deal with mouse motion

--- src/plat/inputs/KeyboardInputs.java
+++ src/plat/inputs/KeyboardInputs.java
@@ -19,18 +19,26 @@ public class KeyboardInputs implements KeyListener
 		switch(pressed.getKeyCode())
 		{
 		
-		case KeyEvent.VK_W:
-			System.out.println("W");
-			break;
-		case KeyEvent.VK_A:
-			System.out.println("A");
-			break;
-		case KeyEvent.VK_S:
-			System.out.println("S");
-			break;
-		case KeyEvent.VK_D:
-			System.out.println("D");
-			break;
+			case KeyEvent.VK_W:
+			{
+				System.out.println("W");
+				break;
+			}	
+			case KeyEvent.VK_A:
+			{
+				System.out.println("A");
+				break;
+			}
+			case KeyEvent.VK_S:
+			{
+				System.out.println("S");
+				break;
+			}
+			case KeyEvent.VK_D:
+			{	
+				System.out.println("D");
+				break;
+			}
 		}
 		
 	}
---
+++ src/plat/inputs/MouseInputs.java
@@ -0,0 +1,52 @@
+package plat.inputs;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class MouseInputs implements MouseListener, MouseMotionListener
+{
+
+	@Override
+	public void mouseDragged(MouseEvent dragged) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent moved) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent clicked) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent pressed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent released) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent entered) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent exited) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
