82d415e461689af9bc2b0ed6954a53ac89c62381
Brickmaster256 <142838286+Brickmaster256@users.noreply.github.com>
2024-04-30 12:33:00 -0600

    created a method that will read teh level data

--- src/plat/utilz/LoadSave.java
+++ src/plat/utilz/LoadSave.java
@@ -1,15 +1,19 @@
 package plat.utilz;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
+import plat.main.Game;
+
 public class LoadSave
 {
 	public static final String PLAYER_ATLAS = "player_sprites.png";
 	public static final String LEVEL_ATLAS = "outside_sprites.png";
+	public static final String LEVEL_ONE_DATA = "level_one_data.png";
 	
 	public static BufferedImage GetSpriteAtlas(String fileName)
 	{
@@ -39,4 +43,21 @@ public class LoadSave
 		
 		return image;
 	}
+	public static int[][] GetLevelData()
+	{
+		int[][] levelData = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
+		BufferedImage image = GetSpriteAtlas(LEVEL_ONE_DATA);
+		
+		for(int height = 0; height < image.getHeight(); height++)
+		{
+			for(int width = 0; width < image.getWidth(); width++)
+			{
+				Color color = new Color(image.getRGB(width,  height));
+				levelData[height][width] = color.getRed();
+			}
+		}
+		
+		return levelData;
+	}
+
 }
