471ac9d876e24dccccc23d7af460fde19cda7915
Brickmaster256 <142838286+Brickmaster256@users.noreply.github.com>
2024-04-30 12:23:00 -0600

    created a loop method to process each individual square

--- src/plat/levels/LevelManager.java
+++ src/plat/levels/LevelManager.java
@@ -9,18 +9,36 @@ import plat.utilz.LoadSave;
 public class LevelManager
 {
 	private Game game;
-	private BufferedImage levelSprite;
+	private BufferedImage[] levelSprite;
 	
 	public LevelManager(Game game)
 	{
 		this.game = game;
-		levelSprite = LoadSave.GetSpriteAtlas(LoadSave.LEVEL_ATLAS);
+//		levelSprite = LoadSave.GetSpriteAtlas(LoadSave.LEVEL_ATLAS);
+		
+		importOutsideSprites();
 	}
 	
 	
+	private void importOutsideSprites()
+	{
+		levelSprite = new BufferedImage[48];
+		BufferedImage image = LoadSave.GetSpriteAtlas(LoadSave.LEVEL_ATLAS);
+		for(int firstIndex = 0; firstIndex < 4; firstIndex++)
+		{
+			for(int secondIndex = 0; secondIndex < 12; secondIndex++)
+			{
+				int index = firstIndex * 12 + secondIndex;
+				levelSprite[index] = image.getSubimage(secondIndex * 32, firstIndex * 32, 32, 32);
+			}
+		}
+		
+	}
+
+
 	public void draw(Graphics graphics)
 	{
-		graphics.drawImage(levelSprite,  0, 0, null);
+		graphics.drawImage(levelSprite[2],  0, 0, null);
 	}
 	public void update()
 	{
