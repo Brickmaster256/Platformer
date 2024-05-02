3a007f6671729fffdee906a2b513421a3d9076e7
Brickmaster256 <142838286+Brickmaster256@users.noreply.github.com>
2024-05-02 13:27:29 -0600

    created a interface for the game state classes

---
+++ src/plat/gamestates/Statemethods.java
@@ -0,0 +1,25 @@
+package plat.gamestates;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public interface Statemethods
+{
+	public void update();
+	
+	public void draw(Graphics graphics);
+	
+	public void mouseClicked(MouseEvent event);
+	
+	public void mouseMoved(MouseEvent event);
+	
+	public void mouseReleased(MouseEvent event);
+	
+	public void mousepressed(MouseEvent event);
+	
+	public void keyPressed(KeyEvent event);
+	
+	public void keyReleased(KeyEvent event);
+	
+}
